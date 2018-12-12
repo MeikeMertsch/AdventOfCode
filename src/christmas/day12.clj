653840343a278b703b96fd71b30e9e5f0dc0e758
nmmertsch@gmail.com <nmmertsch@gmail.com>
2018-12-12 18:02:28 +0100

    solved day 12; not programatically)

--- src/christmas/day12.clj
+++ src/christmas/day12.clj
@@ -1,25 +1,31 @@
 (ns christmas.day12)
 
 (defn init-pots [pots]
-	(str "..." pots "..."))
+	(concat "   " pots "   "))
 
-(defn re-pot [pots instructions]
+(defn re-pot [pots instructions idx]
+	;(spit "resources/Output12" (str idx (apply str pots) "\n") :append true)
 	(->> (partition 5 1 (init-pots pots))
-		 ;(map #(apply str %))
-		 (map #(get instructions % "."))))
+		 (map #(get instructions % \space))))
 	
 
 (defn day12 [init-pots instructions it]
-	(reduce (fn [[pots _] idx] (vector (re-pot pots instructions) idx)) 
+	(reduce (fn [[pots _] idx] (vector (re-pot pots instructions idx) idx)) 
 		[init-pots 0] (range 0 it)))
+
+(defn do-math [pots gen]
+	(->> (zipmap (range (- 0 gen) (+ gen 1000)) pots)
+		 (remove #(= \space (last %)))
+		 (map first)
+		 (apply +)))
+
 
 (defn count-pots [init-pots instructions gen]
 	(->> (day12 init-pots instructions gen)
+		 ;last
 		 first
 		 (#(zipmap (range (- 0 gen) (+ gen 1000)) %))
-		 ;first
-		 (remove #(= \. (last %)))
+		 (remove #(= \space (last %)))
 		 (map first)
-		 (apply +)
-		 ))
+		 (apply +)))
 	
