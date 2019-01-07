b4600b8d4fd202cc9fb15db859253ccca93f44f6
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-07 18:25:35 +0100

    solved day 10 2015, setup day 11

--- src/christmas/chr15/day10.clj
+++ src/christmas/chr15/day10.clj
@@ -1,2 +1,16 @@
 (ns christmas.chr15.day10
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+  			[christmas.tools :as t]))
+
+(defn conf [piece]
+	[(count piece) (first piece)])
+
+(defn tick [number]
+	(->> (partition-by identity number)
+		 (mapcat conf)
+		 (apply str)))
+
+(defn day10 [number times]
+	(->> (iterate tick (str number))
+		 (drop times)
+		 first))
--- test/christmas/chr15/day10_test.clj
+++ test/christmas/chr15/day10_test.clj
@@ -3,4 +3,15 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def file (slurp "resources/christmas/chr15/day10real"))
+(comment 
+(def input 1113122113)
+
+(expect "11" (chr/day10 1 1))
+(expect "21" (chr/day10 1 2))
+(expect "1211" (chr/day10 1 3))
+(expect "111221" (chr/day10 1 4))
+(expect "312211" (chr/day10 1 5))
+
+(expect 360154 (count (chr/day10 input 40)))
+(expect 5103798 (count (chr/day10 input 50)))
+)
