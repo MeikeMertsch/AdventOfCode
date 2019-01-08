e14d4e85c063c35ad072d4269d46dad302c123d5
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-08 20:34:38 +0100

    solved day 16 2015

--- src/christmas/chr15/day16.clj
+++ src/christmas/chr15/day16.clj
@@ -1,2 +1,50 @@
 (ns christmas.chr15.day16
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+  			[christmas.tools :as t]))
+
+(def regex #"goldfish|children|samoyeds|vizslas|perfumes|akitas|pomeranians|cars|trees|cats")
+
+(defn parse-sue [sue]
+	(->> (re-seq regex sue)
+		 (cons "number")
+		 (#(zipmap % (map t/parse-int (re-seq #"\d+" sue))))))
+
+(defn matches? [sue]
+	(and (= (sue "children" 3) 3)
+		 (= (sue "cats" 7) 7)
+		 (= (sue "samoyeds" 2) 2)
+		 (= (sue "pomeranians" 3) 3)
+		 (= (sue "akitas" 0) 0)
+		 (= (sue "vizslas" 0) 0)
+		 (= (sue "goldfish" 5) 5)
+		 (= (sue "trees" 3) 3)
+		 (= (sue "cars" 2) 2)
+		 (= (sue "perfumes" 1) 1)))
+
+(defn matches-2? [sue]
+	(and (= (sue "children" 3) 3)
+		 (> (sue "cats" 100) 7)
+		 (= (sue "samoyeds" 2) 2)
+		 (< (sue "pomeranians" 0) 3)
+		 (= (sue "akitas" 0) 0)
+		 (= (sue "vizslas" 0) 0)
+		 (< (sue "goldfish" 0) 5)
+		 (> (sue "trees" 100) 3)
+		 (= (sue "cars" 2) 2)
+		 (= (sue "perfumes" 1) 1)))
+
+(defn day16 [string]
+	(->> (str/split-lines string)
+		 (map parse-sue)
+		 (filter matches?)
+		 first
+		 (#(% "number"))))
+
+(defn day16b [string]
+	(->> (str/split-lines string)
+		 (map parse-sue)
+		 (filter matches-2?)
+		 first
+		 (#(% "number"))
+		 ))
+
--- test/christmas/chr15/day16_test.clj
+++ test/christmas/chr15/day16_test.clj
@@ -3,4 +3,9 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/christmas/chr15/day16real"))
+
+(expect 213 (chr/day16 file))
+(expect 323 (chr/day16b file))
+)
