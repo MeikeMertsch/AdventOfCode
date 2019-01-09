cd564184e00dda06b77f2dfe1653d05d6fedb038
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-09 20:44:07 +0100

    solved day 18 2015

--- src/christmas/chr15/day18.clj
+++ src/christmas/chr15/day18.clj
@@ -1,2 +1,62 @@
 (ns christmas.chr15.day18
   (:require [clojure.string :as str]))
+
+(def status {\# true \. false})
+
+(defn parse-file [input]
+	(->> (str/split-lines input)
+		 (map-indexed (fn [idx itm] (map-indexed #(vector [%1 idx] (status %2)) itm)))
+		 (apply concat)
+		 (apply concat)
+		 (apply hash-map)))
+
+(defn neighbours [[x0 y0]]
+	(for [x [(dec x0) x0 (inc x0)]
+		  y [(dec y0) y0 (inc y0)]
+		  :when (not (and (= x0 x) (= y0 y)))]
+		[x y]))
+
+(defn on? [status position]
+	(let [glowing-neighbours (count (filter true? (map #(status % false) (neighbours position))))]
+		(if (status position)
+			(some? (#{2 3} glowing-neighbours))
+			(= 3 glowing-neighbours)))) 
+
+(defn tick [status]
+	(->> (keys status)
+		 (sort-by (juxt first last))
+		 (map #(vector % (on? status %)))
+		 (apply concat)
+		 (apply hash-map)))
+
+(defn day18 [input]
+	(->> (parse-file input)
+		 (iterate tick)
+		 (drop 100)
+		 first
+		 vals
+		 (filter true?)
+		 count))
+
+(defn on-2? [status position]
+	(let [glowing-neighbours (count (filter true? (map #(status % false) (neighbours position))))]
+		(if (#{[0 0][0 99][99 0][99 99]} position) true
+		(if (status position)
+			(some? (#{2 3} glowing-neighbours))
+			(= 3 glowing-neighbours))))) 
+
+(defn tick-2 [status]
+	(->> (keys status)
+		 (sort-by (juxt first last))
+		 (map #(vector % (on-2? status %)))
+		 (apply concat)
+		 (apply hash-map)))
+
+(defn day18b [input]
+	(->> (parse-file input)
+		 (iterate tick-2)
+		 (drop 100)
+		 first
+		 vals
+		 (filter true?)
+		 count))
--- test/christmas/chr15/day18_test.clj
+++ test/christmas/chr15/day18_test.clj
@@ -3,4 +3,43 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/christmas/chr15/day18real"))
+
+(def t-state-0 (chr/parse-file 
+".#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####.."))
+
+(def t-state-1 (chr/parse-file 
+"..##..
+..##.#
+...##.
+......
+#.....
+#.##.."))
+
+(expect [[1 1][1 2][1 3][2 1][2 3][3 1][3 2][3 3]] (chr/neighbours [2 2]))
+
+(expect t-state-1 (chr/tick t-state-0))
+
+(def status-2-off {[1 1] true [1 2] true [1 3] false [2 1] false [2 2] false [2 3] false [3 1] false [3 2] false [3 3] false})
+(def status-3-off {[1 1] true [1 2] true [1 3] true [2 1] false [2 2] false [2 3] false [3 1] false [3 2] false [3 3] false})
+(def status-4-off {[1 1] true [1 2] true [1 3] true [2 1] true [2 2] false [2 3] false [3 1] false [3 2] false [3 3] false})
+(def status-2-on {[1 1] true [1 2] true [1 3] false [2 1] false [2 2] true [2 3] false [3 1] false [3 2] false [3 3] false})
+(def status-3-on {[1 1] true [1 2] true [1 3] true [2 1] false [2 2] true [2 3] false [3 1] false [3 2] false [3 3] false})
+(def status-4-on {[1 1] true [1 2] true [1 3] true [2 1] true [2 2] true [2 3] false [3 1] false [3 2] false [3 3] false})
+
+(expect false (chr/on? status-2-off [2 2]))
+(expect true (chr/on? status-3-off [2 2]))
+(expect false (chr/on? status-4-off [2 2]))
+(expect true (chr/on? status-2-on [2 2]))
+(expect true (chr/on? status-3-on [2 2]))
+(expect false (chr/on? status-4-on [2 2]))
+
+;(expect 768 (chr/day18 file))
+;(expect 781 (chr/day18b file))
+)
