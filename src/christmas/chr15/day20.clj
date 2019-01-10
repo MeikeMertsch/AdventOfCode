0a661e1d4804da184ce12c914830adba96180422
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-10 18:22:48 +0100

    Solved day 20 2015

--- src/christmas/chr15/day20.clj
+++ src/christmas/chr15/day20.clj
@@ -1,2 +1,27 @@
 (ns christmas.chr15.day20
   (:require [clojure.string :as str]))
+
+(defn sum-of-divisors [number]
+	(->> (range 1 (Math/floor (inc (Math/sqrt number))))
+		 (filter #(zero? (rem number %)))
+		 (map #(+ % (if-let [x (= % (/ number %))] 0 (/ number %))))
+		 (apply +)))
+
+(defn day20 [input]
+	(->> (range)
+		 (map sum-of-divisors)
+		 (take-while #(< % (/ input 10)))
+		 count))
+
+(defn sum-of-divisors-filtered [number]
+	(->> (range 1 (inc 50))
+		 (map #(vector (/ number %) (rem number %)))
+		 (filter #(= 0 (second %)))
+		 (map first)
+		 (apply +)))
+
+(defn day20c [input]
+	(->> (range)
+		 (map sum-of-divisors-filtered)
+		 (take-while #(< % (/ input 11)))
+		 count))
--- test/christmas/chr15/day20_test.clj
+++ test/christmas/chr15/day20_test.clj
@@ -3,4 +3,13 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-;(def file (slurp "resources/christmas/chr15/day20real"))
+(comment
+(def input 29000000)
+
+(expect 13 (chr/sum-of-divisors 9))
+(expect 1 (chr/sum-of-divisors 1))
+(expect [1 3 4 7 6 12 8 15 13] (map chr/sum-of-divisors (range 1 10)))
+
+;(expect 665280 (chr/day20 input)) ; ~ 40s
+;(expect 665280 (chr/day20c input)) ; ~ 15 s
+)
