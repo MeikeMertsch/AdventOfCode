455cd2504f64c9d8ae4489d48b64fa4f0832b9a8
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-10 21:40:26 +0100

    Solved day 24 2015

--- src/christmas/chr15/day24.clj
+++ src/christmas/chr15/day24.clj
@@ -1,2 +1,37 @@
 (ns christmas.chr15.day24
   (:require [clojure.string :as str]))
+
+(defn compartment-weight [presents compartments]
+	(/ (apply + presents) compartments))
+
+(defn new-combo [all-objects previous]
+	(for [x all-objects
+		  y previous
+		  :let [result (conj y x)]
+		  :when (apply distinct? result)
+		  :when (apply < result)]
+		result))
+
+(defn has-proper-weight? [weight parcels]
+	(= (apply + parcels)
+		weight))
+
+(defn quantum-entanglement [parcels]
+	(apply * parcels))
+
+(defn packing [parcels weight]
+	(->> (iterate (partial new-combo parcels) (map vector parcels))
+		 (drop-while (partial not-any? #(has-proper-weight? weight %)))
+	 	 first
+	 	 (filter (partial has-proper-weight? weight))
+	 	 (sort-by quantum-entanglement)
+	 	 first
+	 	 quantum-entanglement))
+
+(defn day24 [parcels]
+	(packing parcels (compartment-weight parcels 3)))
+
+(defn day24b [parcels]
+	(packing parcels (compartment-weight parcels 4)))
+
+
--- test/christmas/chr15/day24_test.clj
+++ test/christmas/chr15/day24_test.clj
@@ -1,6 +1,20 @@
 (ns christmas.chr15.day24-test
   (:require [christmas.chr15.day24 :as chr]
+  			[christmas.tools :as t]
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def file (slurp "resources/christmas/chr15/day24real"))
+(comment
+(defn get-file [file]
+	(->> (slurp file)
+		 (str/split-lines) 
+		 (map t/parse-int)))
+
+(def file (get-file "resources/christmas/chr15/day24real"))
+
+(expect 520 (chr/compartment-weight file 3))
+(expect 11846773891 (chr/day24 file))
+(expect 80393059 (chr/day24b file))
+
+(expect true (chr/has-proper-weight? 520 [1 4 13 502]))
+)
