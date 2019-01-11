e4c756f545d970c3b07796a38f4ef5ea59b02e94
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-11 22:07:49 +0100

    Solved day 19 2015

--- src/christmas/chr15/day19.clj
+++ src/christmas/chr15/day19.clj
@@ -1,2 +1,38 @@
 (ns christmas.chr15.day19
   (:require [clojure.string :as str]))
+
+(defn parse-instructions [file]
+	(->> (str/split-lines file)
+		 (map #(str/split % #" => "))))
+
+(defn look-for-index [start element string]
+	(str/index-of string element (inc start)))
+
+(defn changed-molecule [instruction string position]
+	(str (subs string 0 position) 
+		 (second instruction) 
+		 (subs string (+ position (count (first instruction))))))
+
+(defn change-molecule [instruction string]
+	(let [molecule (first instruction)]
+	(->> (iterate #(look-for-index % molecule string) 0)
+		 (#(if (str/starts-with? string molecule) % (drop 1 %))) 
+		 (take-while some?)
+		 (map (partial changed-molecule instruction string)))))
+
+(defn day19 [instructions string]
+	(->> (parse-instructions instructions)
+		 (mapcat #(change-molecule % string))
+		 distinct
+		 count))
+
+(defn tick [rev-instructions string]
+	(->> (mapcat #(change-molecule % string) rev-instructions)
+		 first))
+
+(defn day19b [instructions string]
+	(->> (parse-instructions instructions)
+		 (map reverse)
+		 (#(iterate (partial tick %) string))
+		 (take-while (partial not= "e"))
+		 count))
--- test/christmas/chr15/day19_test.clj
+++ test/christmas/chr15/day19_test.clj
@@ -3,5 +3,23 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def input-1 (slurp "resources/christmas/chr15/day19real_1"))
-(def input-2 (slurp "resources/christmas/chr15/day19real_2"))
+(comment
+(def input-instr (slurp "resources/christmas/chr15/day19real_1"))
+(def input-string (slurp "resources/christmas/chr15/day19real_2"))
+
+(expect 506 (count (first (chr/change-molecule (first (chr/parse-instructions input-instr)) input-string))))
+(expect (- 66 14) (count (chr/change-molecule ["Ca" "C"] input-string)))
+(expect 535 (chr/day19 input-instr input-string))
+
+(def t-instr (apply str (interpose "\n" (reverse (str/split-lines "e => H
+e => O
+H => HO
+H => OH
+O => HH")))))
+
+(def t-input "HOH")
+(def t-input-2 "HOHOHO")
+
+(expect 6 (chr/day19b t-instr t-input-2))
+(expect 212 (chr/day19b input-instr input-string))
+)
