e881961548708b07224357f483bf3117d8acccc8
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-12 22:10:33 +0100

    solved day 22 and therefore the whole year 2015

--- src/christmas/chr15/day22.clj
+++ src/christmas/chr15/day22.clj
@@ -1,2 +1,155 @@
 (ns christmas.chr15.day22
   (:require [clojure.string :as str]))
+
+(defn decrease [mana amount]
+	(-> (update mana :amount - amount)
+		(update :steps conj amount)))
+
+(defn magic-missile [game]
+	(-> (update-in game [:player :mana] decrease 53)
+		(update-in [:boss :hp] - 4)))
+
+(defn drain [game]
+	(-> (update-in game [:player :mana] decrease 73)
+		(update-in [:boss :hp] - 2)
+		(update-in [:player :hp] + 2)))
+
+(defn shield [game]
+	(-> (update-in game [:player :mana] decrease 113)
+		(update :effects assoc :shield 6)))
+
+(defn poison [game]
+	(-> (update-in game [:player :mana] decrease 173)
+		(update :effects assoc :poison 6)))
+
+(defn recharge [game]
+	(-> (update-in game [:player :mana] decrease 229)
+		(update :effects assoc :recharge 5)))
+
+(defn apply-shield [game]
+	(update-in game [:effects :shield] dec))
+
+(defn apply-poison [game]
+	(-> (update-in game [:effects :poison] dec)
+		(update-in [:boss :hp] - 3)))
+
+(defn apply-recharge [game]
+	(-> (update-in game [:effects :recharge] dec)
+		(update-in [:player :mana :amount] + 101)))
+
+(def actions {:magic-missile magic-missile :drain drain :shield shield :poison poison :recharge recharge})
+(def actions-cost {53 :magic-missile 73 :drain 113 :shield 173 :poison 229 :recharge})
+(def effects {:shield apply-shield :poison apply-poison :recharge apply-recharge})
+
+(defn apply-effects [game]
+	(->> (:effects game)
+		 (remove (fn [[n c]] (zero? c)))
+		 (map first)
+		 (reduce #((effects %2) %1) game)))
+
+(defn boss-move [game]
+	(let [shielded (< 0 (get-in game[:effects :shield] 0))
+		  boss-damage (get-in game [:boss :damage])
+		  netto-damage (if shielded (- boss-damage 7) boss-damage)]
+		(update-in game [:player :hp] - netto-damage)))
+
+(defn too-costly-actions [game]
+	(let [mana-to-spend (get-in game [:player :mana :amount] 0)]
+		(map last (filter #(< mana-to-spend (first %)) actions-cost))))
+
+(defn running-effects [game]
+	(map first (filter #(pos? (last %)) (:effects game))))
+
+(defn boss-turn [games]
+	(map boss-move games))
+
+(defn remove-losses [games]
+	(remove #(< (get-in % [:player :hp] 0) 1) games))
+
+(defn all-actions [game]
+	(->> (apply dissoc actions (running-effects game))
+		 (#(apply dissoc % (too-costly-actions game)))
+		 vals
+		 (map #(% game))))
+
+(defn player-turn [games]
+	(mapcat all-actions games))
+
+(defn save-wins [status]
+	(let [grouped (group-by #(< (get-in % [:boss :hp]) 1) (:games status))
+		  won (grouped true)
+		  open (grouped false)]
+		(-> (assoc status :wins won)
+			(assoc :games (remove-losses open)))))
+
+(defn take-turn [{:keys [games p-turn] :as status}]
+	(let [after-effects (update status :games (partial map apply-effects))
+		  assigned-turn (update after-effects :p-turn not)]
+		(-> (if p-turn
+				(update assigned-turn :games player-turn)
+				(update assigned-turn :games boss-turn))
+			save-wins)))
+
+(defn day22 [game]
+	(->> (iterate take-turn {:games [game] :p-turn true})
+		 (drop-while #(nil? (:wins %)))
+		 (take-while #(not-empty (:games %)))
+		 (mapcat :wins)
+		 (map (comp :steps :mana :player))
+		 (map (partial apply +))
+		 (apply min)))
+
+(defn remove-one-point [game]
+	(update-in game [:player :hp] dec))
+
+(defn all-actions-hard [game]
+	(->> (apply dissoc actions (running-effects game))
+		 (#(apply dissoc % (too-costly-actions game)))
+		 vals
+		 (map #(% (remove-one-point game)))))
+
+(defn player-turn-hard [games]
+	(mapcat all-actions-hard games))
+
+(defn take-turn-hard [{:keys [games p-turn] :as status}]
+	(let [after-effects (update status :games (partial map apply-effects))
+		  assigned-turn (update after-effects :p-turn not)]
+		(-> (if p-turn
+				(update assigned-turn :games player-turn-hard)
+				(update assigned-turn :games boss-turn))
+			save-wins)))
+
+(defn day22b [game]
+	(->> (iterate take-turn-hard {:games [game] :p-turn true})
+		 (drop-while #(nil? (:wins %)))
+		 (take-while #(not-empty (:games %)))
+		 (mapcat :wins)
+		 (map (comp :steps :mana :player))
+		 (map (partial apply +))
+		 (apply min)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/christmas/chr15/day22_test.clj
+++ test/christmas/chr15/day22_test.clj
@@ -1,6 +1,62 @@
 (ns christmas.chr15.day22-test
   (:require [christmas.chr15.day22 :as chr]
+  		  	[christmas.tools :as t]
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def file (slurp "resources/christmas/chr15/day22real"))
+(comment
+(defn build-map [[hp damage]]
+	{:player {:mana {:amount 500 :steps []} :hp 50} :boss {:hp hp :damage damage}})
+
+(defn parse-file [file]
+	(->> (slurp file)
+		 (re-seq #"\d+")
+		 (map t/parse-int)
+		 build-map))
+
+(def file (parse-file "resources/christmas/chr15/day22real"))
+
+(def game-0 file)
+(def t-game-0 {:player {:mana {:amount 250 :steps []} :hp 10} :boss {:hp 13 :damage 8}})
+(def t-game-1 {:player {:mana {:amount 100 :steps []} :hp 50} :boss {:hp 71 :damage 10}})
+(def game-missile {:player {:mana {:amount 447 :steps [53]}, :hp 50}, :boss {:hp 67, :damage 10}})
+(def game-drain {:player {:mana {:amount 427 :steps [73]}, :hp 52}, :boss {:hp 69, :damage 10}})
+(def game-poison {:player {:mana {:amount 327 :steps [173]}, :hp 50}, :boss {:hp 71, :damage 10} :effects {:poison 6}})
+(def game-shield {:player {:mana {:amount 387 :steps [113]}, :hp 50}, :boss {:hp 71, :damage 10} :effects {:shield 6}})
+(def game-recharge {:player {:mana {:amount 271 :steps [229]}, :hp 50}, :boss {:hp 71, :damage 10} :effects {:recharge 5}})
+
+(def game-0-after-boss {:player {:mana {:amount 500 :steps []} :hp 40} :boss {:hp 71 :damage 10}})
+(def game-shield-after-boss {:player {:mana {:amount 387 :steps [113]}, :hp 47}, :boss {:hp 71, :damage 10} :effects {:shield 6}})
+
+(def game-poison-2 {:player {:mana {:amount 327 :steps [173]}, :hp 50}, :boss {:hp 68, :damage 10} :effects {:poison 5}})
+(def game-shield-2 {:player {:mana {:amount 387 :steps [113]}, :hp 50}, :boss {:hp 71, :damage 10} :effects {:shield 5}})
+(def game-recharge-2 {:player {:mana {:amount 372 :steps [229]}, :hp 50}, :boss {:hp 71, :damage 10} :effects {:recharge 4}})
+
+(expect game-missile (chr/magic-missile game-0))
+(expect game-drain (chr/drain game-0))
+(expect game-shield (chr/shield game-0))
+(expect game-poison (chr/poison game-0))
+(expect game-recharge (chr/recharge game-0))
+
+(expect game-poison-2 (chr/apply-effects game-poison))
+(expect game-shield-2 (chr/apply-effects game-shield))
+(expect game-recharge-2 (chr/apply-effects game-recharge))
+
+(expect game-0-after-boss (chr/boss-move game-0))
+(expect game-shield-after-boss (chr/boss-move game-shield))
+
+(expect [:shield] (chr/running-effects game-shield))
+(expect [:shield :poison :recharge] (chr/too-costly-actions t-game-1))
+
+;(expect 1824 (chr/day22 game-0)) ; 50 s
+(expect 1937 (chr/day22b game-0)) ; 2.5 s
+)
+
+
+
+
+
+
+
+
+
