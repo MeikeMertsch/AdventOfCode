b5ff4701b0af9fb15a9b213ca00571f2fec485d8
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-13 18:42:14 +0100

    solved day 04 2016

--- src/christmas/chr16/day04.clj
+++ src/christmas/chr16/day04.clj
@@ -1,2 +1,52 @@
 (ns christmas.chr16.day04
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+  			[christmas.tools :as t]))
+
+(defn decoy? [room checksum]
+	(->> (str/replace room #"\-" "")
+		 frequencies
+		 (sort-by first #(< (int %2) (int %)))
+		 (sort-by last)
+		 reverse
+		 (map first)
+		 (take 5)
+		 (not= (seq checksum))))
+
+(defn decoy [[room sector checksum]]
+	(if (decoy? room checksum) 0 (t/parse-int sector)))
+
+(defn parse-line [line]
+	(->> (re-seq #"\d+|[a-z\-]+" line)
+		 decoy))
+
+(defn day04 [file]
+	(->> (str/split-lines file)
+		 (map parse-line)
+		 (apply +)))
+
+(def cipher {\a \b \b \c \c \d \d \e \e \f \f \g \g \h \h \i \i \j \j \k \k \l \l \m \m \n \n \o \o \p \p \q \q \r \r \s \s \t \t \u \u \v \v \w \w \x \x \y \y \z \z \a \- \space})
+
+(def value (apply hash-map (apply concat (map-indexed vector (seq "abcdefghijklmnopqrstuvwxyz")))))
+
+(defn next-char [sector character]
+	(-> (int character)
+		(+ sector -97)
+		(rem 26)
+		value))
+
+(defn decrypt [room sector]
+	(->> (map (partial next-char sector) room)
+		 (apply str)))
+
+(defn parse-room [line]
+	(let [room (re-seq #"\d+|[a-z\-]+" line)
+		  room-name (first room)
+		  sector (t/parse-int (second room))] 
+		[(decrypt room-name sector) sector]))
+
+(defn day04b [file]
+	(->> (str/split-lines file)
+		 (map parse-room)
+		 (filter #(str/includes? % "north"))
+		 first
+		 second))
--- test/christmas/chr16/day04_test.clj
+++ test/christmas/chr16/day04_test.clj
@@ -3,4 +3,23 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/chr16/day04real"))
+
+(def real-1 "aaaaa-bbb-z-y-x-123[abxyz]")
+(def real-2 "a-b-c-d-e-f-g-h-987[abcde]")
+(def real-3 "not-a-real-room-404[oarel]")
+(def real-4 "totally-real-room-200[decoy]")
+
+(expect 123 (chr/parse-line real-1))
+(expect 987 (chr/parse-line real-2))
+(expect 404 (chr/parse-line real-3))
+(expect 0 (chr/parse-line real-4))
+
+(expect 158835 (chr/day04 file))
+(expect 993 (chr/day04b file))
+
+(expect ["veryfencryptedfnamef" 343] (chr/parse-room "qzmt-zixmtkozy-ivhz-343"))
+
+(expect \v (chr/next-char 343 \q))
+)
