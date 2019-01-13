4c4e281fae6817ec863ea2cd9abf5620b985d051
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-13 20:21:06 +0100

    solved day 05 2016

--- src/christmas/chr16/day05.clj
+++ src/christmas/chr16/day05.clj
@@ -1,2 +1,51 @@
 (ns christmas.chr16.day05
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+  			[christmas.tools :as t]
+  			[digest :as dig]))
+
+(defn digest [input]
+	(dig/md5 input))
+
+(defn md5 [input number]
+	(digest (str input number)))
+
+(defn build-next-entry [input number]
+	(vector (inc number) (nth (md5 input number) 5)))
+
+(defn next-digit [input [start _]]
+	(println start)
+	(->> (range start (+ 1000000000 start))
+		 (drop-while #(not (str/starts-with? (md5 input %) "00000")))
+		 first
+		 (build-next-entry input)))
+
+(defn day05 [input]
+	(->> (iterate (partial next-digit input) [0 0])
+		 (drop 1)
+		 (take 8)
+		 (map last)
+		 (apply str)))
+
+(defn update-map [m [pos digit]]
+	(if (and (nil? (m pos)) (#{\0 \1 \2 \3 \4 \5 \6 \7} pos)) (assoc m pos digit) m))
+
+(defn build-next-entry-2 [input results number]
+	(->> (md5 input number)
+		 (#(map (partial nth %) [5 6]))
+		 (#(vector (inc number) (update-map results %)))))
+
+(defn next-digit-2 [input [start results]]
+	(println input start results)
+	(->> (range start (+ 1000000000 start))
+		 (drop-while #(not (str/starts-with? (md5 input %) "00000")))
+		 first
+		 (build-next-entry-2 input results)))
+
+(defn day05b [input]
+	(->> (iterate (partial next-digit-2 input) [1469590 {}])
+		 (drop-while #(< (count (second %)) 8))
+		 first
+		 last
+		 (sort-by first)
+		 (map last)
+		 (apply str)))
--- test/christmas/chr16/day05_test.clj
+++ test/christmas/chr16/day05_test.clj
@@ -3,4 +3,17 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/chr16/day05real"))
+
+(def test-door-id "abc")
+(expect "00000155f8105dff7f56ee10fa9b9abd" (chr/md5 "abc" "3231929"))
+
+;(expect [3231930 \1] (chr/next-digit "abc" [0 0]))
+;(expect "18f47a30" (chr/day05 "abc")) ; long running
+;(expect "05ace8e3" (chr/day05b "abc")) ; long running
+
+;(expect "4543c154" (chr/day05 file)) ; ~ 70s
+;(expect-focused "1050cbbd" (chr/day05b file)) ; ~ 190 s 
+
+)
