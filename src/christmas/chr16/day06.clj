030d4406cf8561020403dc27947ad9a70b5a9f23
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-13 20:30:36 +0100

    solved day 06 2016

--- src/christmas/chr16/day06.clj
+++ src/christmas/chr16/day06.clj
@@ -1,2 +1,24 @@
 (ns christmas.chr16.day06
   (:require [clojure.string :as str]))
+
+(defn most-common [column]
+	(->> (frequencies column)
+		 (sort-by last >)
+		 ffirst))
+
+(defn day06 [input]
+	(->> (str/split-lines input)
+		 (apply map vector)
+		 (map most-common)
+		 (apply str)))
+
+(defn least-common [column]
+	(->> (frequencies column)
+		 (sort-by last)
+		 ffirst))
+
+(defn day06b [input]
+	(->> (str/split-lines input)
+		 (apply map vector)
+		 (map least-common)
+		 (apply str)))
--- test/christmas/chr16/day06_test.clj
+++ test/christmas/chr16/day06_test.clj
@@ -4,3 +4,26 @@
             [clojure.string :as str]))
 
 (def file (slurp "resources/chr16/day06real"))
+
+(def test-string 
+	"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar")
+
+(expect "easter" (chr/day06 test-string))
+(expect "ikerpcty" (chr/day06 file))
+(expect "advent" (chr/day06b test-string))
+(expect "uwpfaqrq" (chr/day06b file))
