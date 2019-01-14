ba1423eb1cda4475e9f8937c77e22de3e712c090
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-14 20:09:33 +0100

    solved day 08 2016

--- src/christmas/chr16/day08.clj
+++ src/christmas/chr16/day08.clj
@@ -1,2 +1,65 @@
 (ns christmas.chr16.day08
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+  			[christmas.tools :as t]))
+
+(defn rect [a b _ _ grid]
+	(->> (for [x (range a)
+			   y (range b)]
+			[x y])
+		 (reduce (fn [g coords] (update g coords (constantly true))) grid)))
+
+(defn temp [b [x y] rows grid]
+	(grid [x (rem (+ rows (- y b)) rows)]))
+
+(defn rotate-column [a b _ r grid]
+	(->> (for [x [a]
+			   y (range r)]
+			[x y])
+		 (reduce (fn [g coords] (update g coords (constantly (temp b coords r grid)))) grid)))
+
+(defn temp-2 [b [x y] columns grid]
+	(grid [(rem (+ columns (- x b)) columns) y]))
+
+(defn rotate-row [a b c _ grid]
+	(->> (for [x (range c)
+			   y [a]]
+			[x y])
+		 (reduce (fn [g coords] (update g coords (constantly (temp-2 b coords c grid)))) grid)))
+
+(def instructions {"rect" rect "rotate c" rotate-column "rotate r" rotate-row})
+
+(defn parse-pieces [[instr a b]]
+	[(instructions instr)
+	 (t/parse-int a)
+	 (t/parse-int b)])
+
+(defn parse-line [line]
+	(->> (re-seq #"rect|rotate c|rotate r|\d+" line)
+		 parse-pieces))
+
+(defn initiate-grid [columns rows]
+	(-> (for [x (range columns)
+		  	  y (range rows)]
+			[x y])
+		 (zipmap (repeat false))))
+
+(defn parse-file [input]
+	(->> (str/split-lines input)
+		 (map parse-line)))
+
+(defn apply-instruction [c r grid [instr a b]]
+	(instr a b c r grid))
+
+(defn day08 [input columns rows]
+	(->> (parse-file input)
+		 (reduce (partial apply-instruction columns rows) (initiate-grid columns rows))
+		 vals
+		 (filter true?)
+		 count))
+
+(defn create-line [columns grid row]
+	(println (map {true "#" false " "} (map #(grid [% row]) (range columns)))))
+
+(defn day08b [input columns rows]
+	(let [grid (->> (parse-file input) (reduce (partial apply-instruction columns rows) (initiate-grid columns rows)))]
+		(map (partial create-line columns grid) (range rows))))
--- test/christmas/chr16/day08_test.clj
+++ test/christmas/chr16/day08_test.clj
@@ -3,4 +3,23 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/chr16/day08real"))
+
+(def t-file "rect 3x2
+rotate column x=1 by 1
+rotate row y=0 by 4
+rotate column x=1 by 1")
+
+(def t-status-1 {[2 2] false, [0 0] true, [1 0] true, [1 1] true, [4 2] false, [3 0] false, [4 1] false, [5 2] false, [5 1] false, [6 1] false, [0 2] false, [2 0] true, [3 1] false, [2 1] true, [5 0] false, [6 2] false, [6 0] false, [1 2] false, [3 2] false, [0 1] true, [4 0] false})
+(def t-status-2 {[2 2] false, [0 0] true, [1 0] false, [1 1] true, [4 2] false, [3 0] false, [4 1] false, [5 2] false, [5 1] false, [6 1] false, [0 2] false, [2 0] true, [3 1] false, [2 1] true, [5 0] false, [6 2] false, [6 0] false, [1 2] true, [3 2] false, [0 1] true, [4 0] false})
+(def t-status-3 {[2 2] false, [0 0] false, [1 0] false, [1 1] true, [4 2] false, [3 0] false, [4 1] false, [5 2] false, [5 1] false, [6 1] false, [0 2] false, [2 0] false, [3 1] false, [2 1] true, [5 0] false, [6 2] false, [6 0] true, [1 2] true, [3 2] false, [0 1] true, [4 0] true})
+
+(expect t-status-2 (chr/rotate-column 1 1 7 3 t-status-1))
+(expect t-status-3 (chr/rotate-row 0 4 7 3 t-status-2))
+
+(expect 6 (chr/day08 t-file 7 3))
+(expect 123 (chr/day08 file 50 6))
+
+(expect [nil nil nil nil nil nil] (chr/day08b file 50 6))
+)
