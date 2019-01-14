a70d60030bf25bec6407fe46f1671fcfdc9c43f8
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-01-14 17:54:26 +0100

    solved day 07 2016

--- src/christmas/chr16/day07.clj
+++ src/christmas/chr16/day07.clj
@@ -1,2 +1,60 @@
 (ns christmas.chr16.day07
   (:require [clojure.string :as str]))
+
+(def regex-hyper #"\[|\]")
+
+(defn contains-abba? [entry]
+	(->> (re-find #"(\w)(\w)\2\1" entry)
+		 first
+		 (#(not= (first %) (second %)))))
+
+(defn supports-tls [line]
+	(->> (cons false line)
+		 (partition 2)
+		 (apply map vector)
+		 (#(and (every? false? (first %))
+		 		(some true? (second %))))))
+
+(defn parse-line [line]
+	(->> (str/split line regex-hyper)
+		 (map contains-abba?)))
+
+(defn day07 [input]
+	(->> (str/split-lines input)
+		 (map parse-line)
+		 (filter supports-tls)
+		 count))
+
+(defn triples [part]
+	(->> (partition 3 1 part)
+		 (filter #(= (first %) (last %)))
+		 (filter #(not= (first %) (second %)))))
+
+(defn divide-hyper [net]
+	(->> (cons [] net)
+		 (partition 2)
+		 (apply map vector)))
+
+(defn compare-ssl-candidates [[hyper outside]]
+	(for [[a b c] hyper
+		  [d e f] outside
+		  :when (= b d f)
+		  :when (= a c e)]
+		  [a b c d e f]))
+
+(defn supports-ssl? [net]
+	(->> (map triples net)
+		 divide-hyper
+		 (map (partial apply concat))
+		 compare-ssl-candidates
+		 first
+		 some?))
+
+(defn parse-net [net]
+	(->> (str/split-lines net)
+		 (map #(str/split % regex-hyper))))
+
+(defn day07b [input]
+	(->> (parse-net input)
+		 (filter supports-ssl?)
+		 count))
--- test/christmas/chr16/day07_test.clj
+++ test/christmas/chr16/day07_test.clj
@@ -3,4 +3,24 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/chr16/day07real"))
+(def test-tfft "abba[mnop]qrst
+abcd[bddb]xyyx
+aaaa[qwer]tyui
+ioxxoj[asdfgh]zxcvbn")
+
+(def testb-tftt 
+"aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb")
+
+(expect 2 (chr/day07 test-tfft))
+(expect 110 (chr/day07 file))
+
+(expect [true false true true] (map chr/supports-ssl? (chr/parse-net testb-tftt)))
+
+(expect 3 (chr/day07b testb-tftt))
+(expect 242 (chr/day07b file))
+)
