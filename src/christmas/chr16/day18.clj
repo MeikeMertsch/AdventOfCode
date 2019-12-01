984ba252f574ceb3e0b512d5a58410b5067c41c4
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-12-01 17:59:39 +0100

    2016 day 18

--- src/christmas/chr16/day18.clj
+++ src/christmas/chr16/day18.clj
@@ -1,3 +1,22 @@
 (ns christmas.chr16.day18
   (:require [clojure.string :as str]))
 
+(defn trap? [previous-tiles]
+   (get {["^" "^" "."] "^" ["." "^" "^"] "^" ["^" "." "."] "^" ["." "." "^"] "^"} previous-tiles "."))
+
+(defn next-row [previous-row]
+		 (->> (lazy-cat ["."] previous-row ["."])
+		 		   (partition 3 1)
+		 		   (map trap?)
+		 )
+)
+
+(defn tile-room [input room-length]
+	  (->> (seq input)
+	  					(map str)
+	  					(iterate next-row)
+	  	    (take room-length)
+	  	    flatten
+	  	    (filter #(= "." %))
+	  	    count
+	  ))
--- test/christmas/chr16/day18_test.clj
+++ test/christmas/chr16/day18_test.clj
@@ -4,3 +4,20 @@
             [clojure.string :as str]))
 
 (def file (slurp "resources/chr16/day18real"))
+
+(comment
+(expect "^" (chr/trap? ["^" "^" "."]))
+(expect "^" (chr/trap? ["." "^" "^"]))
+(expect "^" (chr/trap? ["^" "." "."]))
+(expect "^" (chr/trap? ["." "." "^"]))
+(expect "." (chr/trap? ["^" "." "^"]))
+(expect "." (chr/trap? ["." "^" "."]))
+(expect "." (chr/trap? ["^" "^" "^"]))
+(expect "." (chr/trap? ["." "." "."]))
+
+(expect ["." "^" "^" "^" "^"] (chr/next-row ["." "." "^" "^" "."]))
+
+(expect 38 (chr/tile-room ".^^.^.^^^^" 10)) 
+(expect 1926 (chr/tile-room file 40)) 
+(expect 1996597 (chr/tile-room file 400000)) 
+)
