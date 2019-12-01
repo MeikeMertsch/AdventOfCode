d8cc06130921fb1572b042495deb516cd46af318
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-12-01 19:29:15 +0100

    2016 day 15

--- src/christmas/chr16/day15.clj
+++ src/christmas/chr16/day15.clj
@@ -1,2 +1,30 @@
 (ns christmas.chr16.day15
-  (:require [clojure.string :as str]))
+    (:require [christmas.tools :as tools])
+    (:require [clojure.string :as str]))
+
+
+(defn parse-file [file]
+	   (->> (str/split-lines file)
+	        (map (partial re-seq #"\d+"))
+	        (map (partial map tools/parse-int))))
+
+(defn cyc[[number slots _ start-pos]]
+    (->> (range slots)
+         cycle
+         (drop start-pos)
+         (drop number)))
+
+(defn get-time [file]
+    (->> (parse-file file)
+    					(map cyc)
+    					(apply map +)
+    					(take-while pos?)
+    					count))
+
+(defn get-time2 [file]
+    (->> (parse-file file)
+         (cons [7 11 0 0])
+    					(map cyc)
+    					(apply map +)
+    					(take-while pos?)
+    					count))
--- test/christmas/chr16/day15_test.clj
+++ test/christmas/chr16/day15_test.clj
@@ -4,3 +4,13 @@
             [clojure.string :as str]))
 
 (def file (slurp "resources/chr16/day15real"))
+
+(comment
+(expect [[1 17 0 15] [2 3 0 2] [3 19 0 4] [4 13 0 2] [5 7 0 2] [6 5 0 0]] 
+    (chr/parse-file file))
+
+(expect [0 1 2 3 4 5 6 0 1 2 3 4 5 6 0 1 2 3 4 5] (take 20 (chr/cyc [5 7 0 2])))
+
+(expect 400589 (chr/get-time file))
+(expect 400589 (chr/get-time2 file))
+)
