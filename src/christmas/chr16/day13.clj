e089372590a2d3dc7d11ede70e6b7a5837ebc810
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-12-01 17:59:11 +0100

    2016 day 13

--- src/christmas/chr16/day13.clj
+++ src/christmas/chr16/day13.clj
@@ -1,2 +1,49 @@
 (ns christmas.chr16.day13
-  (:require [clojure.string :as str]))
+	   (:require [clojure.string :as str]))
+
+(defn valid? [blocked [x y :as value]]
+    (and (<= 0 x)
+         (<= 0 y)
+         (not (contains? blocked value))))
+
+(defn neighbors[value]
+				(map (partial map +) (repeat value) [[0 1][1 0][0 -1][-1 0]]))
+
+(defn find-possibilities[{:keys [actual blocked]}]
+    (->> (map neighbors actual)
+         (apply concat)
+         (filter (partial valid? blocked))))
+
+(defn way? [fav [x y]]
+    (->> (+ (* x x) (* 3 x) (* 2 x y) y (* y y) fav)
+							  Integer/toBinaryString
+							  (filter (partial = \1))
+							  count
+							  even?))      
+
+(defn tick [{:keys [fav] :as game}]
+				(let [poss (find-possibilities game)
+				      ways (filter (partial way? fav) poss)]
+								(-> (update-in game [:blocked] #(reduce conj % poss))
+								    (assoc :actual ways)
+								    (update-in [:ways] #(reduce conj % ways)))))
+
+(defn not-goal[{:keys [blocked goal]}]
+				(not (contains? blocked goal)))
+
+(defn walk [input goal]
+    (->> {:actual [[1 1]] :blocked #{} :fav input :goal goal :ways #{[1 1]}}
+    					(iterate tick)
+    					(take-while not-goal)
+    					count))
+
+
+(defn walk2 [input goal]
+    (->> {:actual [[1 1]] :blocked #{} :fav input :goal goal :ways #{[1 1]}}
+    					(iterate tick)
+    					(drop 50)
+    					first
+    					:ways
+    					count))
+
+
--- test/christmas/chr16/day13_test.clj
+++ test/christmas/chr16/day13_test.clj
@@ -3,4 +3,31 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def file (slurp "resources/chr16/day13real"))
+(def t-input 10)
+(def t-goal [7 4])
+
+(def input 1362)
+(def goal [31 39])
+
+(comment
+(expect 11 (chr/walk t-input t-goal))
+;(expect 82 (chr/walk input goal))
+
+(expect 138 (chr/walk2 input goal)) ;; 173 too high ;134 too low
+
+(expect \1 (first (Integer/toBinaryString 3)))
+
+(expect false (chr/valid? #{[1 5] [2 5]} [2 5]))
+(expect false (chr/valid? #{[1 5] [2 5]} [-2 5]))
+(expect true (chr/valid? #{[1 5] [2 5]} [3 5]))
+
+(expect #{[0 1][1 0][2 1][1 2]} (set (chr/neighbors [1 1])))
+
+(expect #{[0 1][1 0][2 1][1 2]} (set (chr/find-possibilities {:actual [[1 1]] :blocked #{} :fav input :goal goal})))
+
+
+(expect true (chr/way? 10 [1 1]))
+(expect false (chr/way? 10 [4 0]))
+(expect false (chr/way? 10 [9 0]))
+(expect true (chr/way? 10 [6 6]))
+)
