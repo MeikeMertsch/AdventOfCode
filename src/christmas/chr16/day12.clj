5411b4baa40ad38e43c2af55bc6b51f746c25f54
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-12-02 21:03:10 +0100

    2016 day 12

--- src/christmas/chr16/day12.clj
+++ src/christmas/chr16/day12.clj
@@ -1,2 +1,46 @@
 (ns christmas.chr16.day12
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+  										[christmas.tools :as tools]))
+
+(def regs {"a" 0 "b" 1 "c" 2 "d" 3})
+
+(defn get-reg-or-nr [value registers]
+    (let [value-in-reg (regs value)]
+    					(if value-in-reg (get registers value-in-reg) (tools/parse-int value))))
+
+(defn ccpy [registers x y]
+				(update-in registers [(regs y)] (constantly (get-reg-or-nr x registers))))
+
+(defn cinc [registers x]
+				(update-in registers [(regs x)] inc))
+
+(defn cdec [registers x]
+				(update-in registers [(regs x)] dec))
+
+(defn cjnz [registers x y]
+    (if (not= 0 (get-reg-or-nr x registers))
+        (update-in registers [4] (partial + (tools/parse-int y) -1))
+        registers))
+
+(def functions {"cpy" ccpy "jnz" cjnz "dec" cdec "inc" cinc})
+
+(defn parse-file [file]
+				(->> (str/split-lines file)
+				     (map (partial re-seq #"-?\d+|cpy|jnz|inc|dec|\w"))))
+
+(defn thing [instructions registers]
+	   (-> (nth instructions (last registers))
+	       (#(apply (partial (functions (first %)) registers) (rest %)))
+	       (update-in [4] inc)))
+
+(defn process [file c]
+    (let [instructions (parse-file file)]
+    (->> (iterate (partial thing instructions) [0 0 c 0 0])
+         ;(take 1025)
+         (drop-while #(< (last %) (count instructions)))
+         first
+
+
+    )))
+
+
--- test/christmas/chr16/day12_test.clj
+++ test/christmas/chr16/day12_test.clj
@@ -3,4 +3,17 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/chr16/day12real"))
+
+(expect "" (chr/parse-file file))
+
+(expect [1 2 3 4 1] (chr/ccpy [1 2 0 4 1] "3" "c"))
+(expect [1 2 2 4 2] (chr/ccpy [1 2 0 4 2] "b" "c"))
+
+(expect [1 2 0 4 2] (chr/cjnz [1 2 0 4 2] "c" "3"))
+(expect [1 2 3 4 4] (chr/cjnz [1 2 3 4 2] "b" "3"))
+
+;(expect 317993 (chr/process file 0))
+;(expect 9227647 (chr/process file 1))
+)
