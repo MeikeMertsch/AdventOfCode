c32be6bb0f0d5d48e2b2c1ada1ddf90542c35513
nmmertsch@gmail.com <nmmertsch@gmail.com>
2019-12-02 19:10:00 +0100

    2016 day 14

--- src/christmas/chr16/day14.clj
+++ src/christmas/chr16/day14.clj
@@ -1,2 +1,41 @@
 (ns christmas.chr16.day14
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [digest :as dig]))
+
+(defn digest [input digestions]
+    (->> (iterate dig/md5 input)
+         (drop digestions)
+         first))
+
+(defn contains-triple?[code]
+				(->> (partition 3 1 code)
+									(filter (partial apply =))
+									ffirst))
+
+(defn contains-5tuple?[code]
+				(->> (partition 5 1 code)
+									(filter (partial apply =))
+									(map first)))
+
+(defn process-tuple[number {:keys [pad-keys candidates] :as knowledge} tuple]
+				(let [happy-candidates (map first (filter (comp #{tuple} last) candidates))]
+  		    (-> (update-in knowledge [:pad-keys] #(take 64 (sort (concat % happy-candidates))))
+  		        (update-in [:candidates] #(apply dissoc % happy-candidates)))))
+
+
+(defn process-next[{:keys [candidates hash digestions] :as knowledge} number]
+	   (let [code (digest (str hash number) digestions)
+	   						triple (contains-triple? code)
+	         tuples (contains-5tuple? code)]
+	       (-> (if (empty? tuples) knowledge (reduce (partial process-tuple number) knowledge tuples))
+	           (#(if triple (assoc-in % [:candidates number] triple) %))
+	   	       (update-in [:candidates] #(dissoc % (- number 1001))))))
+
+(defn process[input digestions]
+				(->> (reductions process-next {:pad-keys [] :candidates {} :hash input :digestions digestions} (range))
+				     (drop-while #(or (< (count (:pad-keys %)) 64) 
+				     			              (< (first (sort (keys (:candidates %)))) (last (:pad-keys %)))))
+				     first
+				     :pad-keys
+				     last))
+
--- test/christmas/chr16/day14_test.clj
+++ test/christmas/chr16/day14_test.clj
@@ -3,4 +3,24 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def file (slurp "resources/chr16/day14real"))
+(comment
+(def input "jlmsuwbz")
+(def t-input "abc")
+
+;(expect "" (chr/get-64-keys input))
+
+(expect \o (chr/contains-triple? "efajkshdfoooajh"))
+(expect nil (chr/contains-triple? "efajkshdfooajh"))
+(expect \j (chr/contains-triple? "efajjjjkshdfoooajh"))
+
+(expect [\o] (chr/contains-5tuple? "efajjjkshdfoooooajh"))
+(expect [\j \o] (chr/contains-5tuple? "efajjjjjkshdfoooooajh"))
+
+(expect {:pad-keys [] :candidates {18 \8}, :hash "abc" :digestions 1} 
+        (chr/process-next {:pad-keys [] :candidates {} :hash t-input :digestions 1} 18))
+(expect {:pad-keys [39], :candidates {816 \e}, :hash "abc" :digestions 1} 
+        (chr/process-next {:pad-keys [] :candidates {39 \e} :hash t-input :digestions 1} 816))
+
+;(expect 35186 (chr/process input 1))  ; 22728 too low ; 35408 too high
+;(expect 22429 (chr/process input 2017))  
+)
