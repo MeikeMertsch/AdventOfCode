1f2a71ef452fcd347c874b0e074d70fca5dfb37c
MeikeMertsch <nmmertsch@gmail.com>
2020-12-29 15:32:29 +0100

    re-did 2019/04

--- src/christmas/chr19/day04.clj
+++ src/christmas/chr19/day04.clj
@@ -1,2 +1,33 @@
 (ns christmas.chr19.day04
-	(:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [christmas.tools :as tt]))
+
+(def basic-pred #(<= 2 %))
+(def strict-pred #{2})
+
+(defn parse [file]
+  (->> (slurp file)
+       (re-seq #"\d+")
+       (map tt/parse-int)))
+
+(defn password? [pred number]
+  (->> (str number)
+       (map #(tt/parse-int (str %)))
+       (#(and
+          (apply <= %)
+          (->> (partition-by identity %) 
+               (map count) 
+               (some pred))))))
+
+(defn count-valids [pred numbers]
+  (->> (apply range numbers)
+       (filter (partial password? pred))
+       (count)))
+
+(defn day04 [input]
+  (->> (parse input)
+       (count-valids basic-pred)))
+
+(defn day04b [input]
+  (->> (parse input)
+       (count-valids strict-pred)))
--- test/christmas/chr19/day04_test.clj
+++ test/christmas/chr19/day04_test.clj
@@ -1,14 +1,22 @@
-(ns christmas.chr17.day04-test
- (:require [christmas.chr17.day04 :as chr]
- 		 	[expectations :refer :all]
- 		 	[clojure.string :as str]))
+(ns christmas.chr19.day04-test
+  (:require [christmas.chr19.day04 :as chr]
+            [expectations :refer :all]
+            [clojure.string :as str]))
+
 (comment
-(defn get-file [file-name]
-	(->> (slurp file-name)
-		 str/split-lines
-		 (map #(str/split % #" "))))
+  (def realfile "resources/chr19/day04real")
 
-(def file (get-file "resources/chr17/day04real"))
+  (expect 594 (chr/day04 realfile))
 
-(expect 119 (chr/day04 file))
-)
+  (defn basic-pw [number] (chr/password? chr/basic-pred number))
+  (defn strict-pw [number] (chr/password? chr/strict-pred number))
+
+  (expect true (boolean (basic-pw 111111)))
+  (expect false (boolean (basic-pw 223450)))
+  (expect false (boolean (basic-pw 123789)))
+
+  (expect 364 (chr/day04b realfile))
+
+  (expect true (boolean (strict-pw 112233)))
+  (expect false (boolean (strict-pw 123444)))
+  (expect true (boolean (strict-pw 111122))))
