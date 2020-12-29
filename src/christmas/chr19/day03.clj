82611de45588484b3059c9c9c8139b99b0601821
MeikeMertsch <nmmertsch@gmail.com>
2020-12-29 10:07:29 +0100

    re-did 2019/03

--- src/christmas/chr19/day03.clj
+++ src/christmas/chr19/day03.clj
@@ -1,2 +1,58 @@
 (ns christmas.chr19.day03
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as s]
+            [christmas.tools :as t]))
+
+(def directions {"L" [-1 0] "R" [1 0] "U" [0 1] "D" [0 -1]})
+
+(defn parse-instruction [[direction length]]
+  (vector (directions direction) (t/parse-int length)))
+
+(defn parse-wire [wire]
+  (->> (str/split wire #",")
+       (map (partial re-seq #"\d+|\w"))
+       (map parse-instruction)))
+
+(defn parse [file]
+  (->> (slurp file)
+       (str/split-lines)
+       (map parse-wire)))
+
+(defn walk [path [direction length]]
+  (->> (peek path)
+       (iterate #(mapv + direction %))
+       (rest)
+       (take length)
+       (into path)))
+
+(defn walk-instructions [instructions]
+  (reduce walk [[0 0]] instructions))
+
+(defn intersections [paths]
+  (->> (map set paths)
+       (apply s/intersection)))
+
+(defn day03 [input]
+  (->> (parse input)
+       (map walk-instructions)
+       (map rest)
+       (intersections)
+       (map t/manhatten)
+       (apply min)))
+
+(defn sketch-distances [path]
+  (->> (map-indexed #(vector %2 %1) path)
+       (apply concat)
+       (apply hash-map)))
+
+(defn find-fastest-intersection [distances]
+  (->> (map #(dissoc % [0 0]) distances)
+       (apply merge-with vector)
+       (keep #(when (vector? (peek %)) (apply + (peek %))))
+       (apply min)))
+
+(defn day03b [input]
+  (->> (parse input)
+       (map walk-instructions)
+       (map sketch-distances)
+       (find-fastest-intersection)))
--- test/christmas/chr19/day03_test.clj
+++ test/christmas/chr19/day03_test.clj
@@ -1,7 +1,21 @@
-(ns christmas.chr17.day03-test
-  (:require [christmas.chr17.day03 :as chr]
-  		  	[expectations :refer :all]
+(ns christmas.chr19.day03-test
+  (:require [christmas.chr19.day03 :as chr]
+            [expectations :refer :all]
             [clojure.string :as str]))
+
 (comment
-(def file (slurp "resources/chr17/day03real"))
-)
+  (def file-a "resources/chr19/day03")
+  (def file-b "resources/chr19/day03b")
+  (def file-c "resources/chr19/day03c")
+  (def realfile "resources/chr19/day03real")
+
+  (expect 6 (chr/day03 file-a))
+  (expect 159 (chr/day03 file-b))
+  (expect 135 (chr/day03 file-c))
+;(expect 1017 (chr/day03 realfile))
+
+  (expect 30 (chr/day03b file-a))
+  (expect 610 (chr/day03b file-b))
+  (expect 410 (chr/day03b file-c))
+;(expect 11432 (chr/day03b realfile))
+  )
